8951ea0e560fa2e5df57e7940ef3faf56d29af07
m-triple-m <triplem656@gmail.com>
2022-05-02 16:43:31 +0530

    ..

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
